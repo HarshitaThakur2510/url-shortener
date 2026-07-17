6d54e50ad93cd7a48f1aafdd73b516b6053be8d9
harshita <harshita0336.becse24@chitkara.edu.in>
2026-07-17 23:47:18 +0530

    Refactor UrlController for clarity and remove comments

--- src/main/java/com/harshita/urlShortener/controller/UrlController.java
+++ src/main/java/com/harshita/urlShortener/controller/UrlController.java
@@ -10,39 +10,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController // handles HTTP req and return JSON..Without it, Spring won't recognize this class as a controller
-@RequestMapping("/api/urls") // this is base url
+@RestController 
+@RequestMapping("/api/urls")
 public class UrlController {
 
     private final UrlService urlService;
 
-    public UrlController(UrlService urlService) { //connects controller to services
+    public UrlController(UrlService urlService) {
         this.urlService = urlService;
     }
 
-    @PostMapping // method handles POST /api/urls
+    @PostMapping
     public ResponseEntity<UrlResponse> shortenUrl(
-            @Valid @RequestBody UrlRequest request) { // automatically converts this JSON into UrlRequest
-
+            @Valid @RequestBody UrlRequest request) { 
         UrlResponse response = urlService.createShortUrl(request);
 
-        return new ResponseEntity<>(response, HttpStatus.CREATED); // helps us control response body, http
+        return new ResponseEntity<>(response, HttpStatus.CREATED); 
     }
 
-//    @GetMapping("/{shortCode}") // /(value of shortcode)
-//    // if someone visits site using short code this method open the link to which short code is assigned
-//    // this method returns httpStatus code and loc header if url is found then hhtp code is 320 and in loc header org url is passed
-//    public ResponseEntity<Void> redirectToOriginalUrl(
-//            @PathVariable String shortCode ) { // pathVairable extracts shortcode from link eg:localhost:8080/XYZ99 it will extract XYZ99
-//
-//        String originalUrl = urlService.getOriginalUrl(shortCode); // looks for orignal url acc to short code
-//
-//        return ResponseEntity.status(HttpStatus.FOUND)
-//                .location(URI.create(originalUrl))// adds org url to location header in hhtp response
-//                .build(); //send response to client.. i am done building the response
-//    }
 
-    @GetMapping("/details/{shortCode}") // mapping for url details
+    @GetMapping("/details/{shortCode}") 
     public ResponseEntity<UrlDetailsResponse> getUrlDetails(
             @PathVariable String shortCode) {
 
