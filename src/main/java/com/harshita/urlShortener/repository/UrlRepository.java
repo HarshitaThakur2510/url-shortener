8e5162cf0a8e72eb4b65e2a298257236f8083a29
harshita <harshita0336.becse24@chitkara.edu.in>
2026-07-17 23:52:21 +0530

    Remove unnecessary comment in UrlRepository
    Removed comment explaining the use of interface in UrlRepository.

--- src/main/java/com/harshita/urlShortener/repository/UrlRepository.java
+++ src/main/java/com/harshita/urlShortener/repository/UrlRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-// we used interface instead of class becoz spring generates functions automatically like save() delete().... JpaRepository<Url, Long> creates url entity whose primary key is long
+
 public interface UrlRepository extends JpaRepository<Url, Long> {
 
     Optional<Url> findByShortCode(String shortCode);
