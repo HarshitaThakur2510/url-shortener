2cb309ebf8c57f616a914ca974495e518c21996a
harshita <harshita0336.becse24@chitkara.edu.in>
2026-07-17 23:55:19 +0530

    Refactor Base62Generator by cleaning comments and formatting
    Removed unnecessary comment and cleaned up code formatting.

--- src/main/java/com/harshita/urlShortener/util/Base62Generator.java
+++ src/main/java/com/harshita/urlShortener/util/Base62Generator.java
@@ -1,5 +1,4 @@
 package com.harshita.urlShortener.util;
-//we used Base62 because if we used only no. then it would become very long so we use A-Z a-z 0-9 that is 62 characters
 import org.springframework.stereotype.Component;
 
 import java.security.SecureRandom;
@@ -12,15 +11,14 @@ public class Base62Generator {
 
     private static final int SHORT_CODE_LENGTH = 6;
 
-    private final SecureRandom random = new SecureRandom(); // used to generate random values and it is better that random
-
+    private final SecureRandom random = new SecureRandom(); 
     public String generateShortCode() {
 
         StringBuilder shortCode = new StringBuilder();
 
         for (int i = 0; i < SHORT_CODE_LENGTH; i++) {
 
-            int index = random.nextInt(CHARACTERS.length()); //random.nextInt(62): a random value between 0-61 will be stored in index
+            int index = random.nextInt(CHARACTERS.length()); 
 
             shortCode.append(CHARACTERS.charAt(index));
         }
