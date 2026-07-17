e689c36fd1bd7bc53924089efff860eb524d8889
harshita <harshita0336.becse24@chitkara.edu.in>
2026-07-17 23:51:10 +0530

    Refactor UrlResponse class by cleaning comments
    Removed unnecessary comments and adjusted class declaration.

--- src/main/java/com/harshita/urlShortener/dto/UrlResponse.java
+++ src/main/java/com/harshita/urlShortener/dto/UrlResponse.java
@@ -1,11 +1,10 @@
 package com.harshita.urlShortener.dto;
-// we created response dto so that we only give client, url and short code
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class UrlResponse { //return org url and short code
+public class UrlResponse {
 
     private String originalUrl;
 
